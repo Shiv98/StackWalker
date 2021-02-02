d691d5680c487bde70a6bb8d2645a2432adb4fc8
Shiv98 <shivangi.gupta9820@gmail.com>
2021-02-02 14:09:20 +0530

    Call Stack for same process with Merge Call Tree

--- Main/StackWalker/GraphCreator.cpp
+++ Main/StackWalker/GraphCreator.cpp
@@ -144,9 +144,9 @@ CallTreeNode MergeNodes(CallTreeNode* root)
         }
         //root->children.erase(root->children.begin() + i);
       }
-      for (size_t i = duplicateIndices.size(); i >=1; i--)
+      for (size_t i = duplicateIndices.size(); i >1; i--)
       {
-        root->children.erase(root->children.begin() + duplicateIndices[i]);
+        root->children.erase(root->children.begin() + duplicateIndices[i-1]);
       }
       duplicateIndices.clear();
     }
@@ -177,7 +177,7 @@ CallTreeNode* CreateTree(std::string key, std::vector<std::vector<std::string>> 
   }
 
   // Merge nodes in branch.
-  //*root = MergeNodes(root);
+  *root = MergeNodes(root);
 
   *root = SetPercentages(root, root->count);
 
